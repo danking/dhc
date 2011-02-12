f5f1459d648580acad197f8463ea683815825fdf
Daniel King <danking@ccs.neu.edu>
2011-02-12 18:53:55 -0500

    Actually wrote something

--- main.hs
+++ main.hs
@@ -1,1 +1,33 @@
 -- Durham Haskell Compiler
+import qualified Data.Map as M
+
+main =
+  return ()
+
+data Expr = Lit Integer
+          | Id String
+          | Func Expr Expr
+          | Apply Expr Expr
+          deriving (Show)
+
+type Env = M.Map String Result
+
+data Result = RInt Integer
+            | Closure Expr Expr Env
+            deriving (Show)
+
+eval :: Expr -> Env -> Maybe Result
+eval (Lit x) _ = Just $ RInt x
+eval (Id x) env = M.lookup x env
+eval (Func a b) env = Just $ Closure a b env
+eval (Apply f p) env =
+    case eval p env of
+      Nothing -> Nothing
+      Just param -> case eval f env of
+                      Nothing -> Nothing
+                      Just (Closure arg body closed_env) ->
+                          case arg of
+                            Id x -> eval body $ M.insert x param closed_env
+                            _ -> Nothing
+
+
