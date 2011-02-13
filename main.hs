8b8472e74ac073cf6f1582833d388718dad797a7
Patrick MacArthur <generalpenguin89@gmail.com>
2011-02-12 23:41:34 -0500

    Made the eval (Apply _) code a bit more monadic

--- main.hs
+++ main.hs
@@ -20,14 +20,10 @@ eval :: Expr -> Env -> Maybe Result
 eval (Lit x) _ = Just $ RInt x
 eval (Id x) env = M.lookup x env
 eval (Func a b) env = Just $ Closure a b env
-eval (Apply f p) env =
-    case eval p env of
-      Nothing -> Nothing
-      Just param -> case eval f env of
-                      Nothing -> Nothing
-                      Just (Closure arg body closed_env) ->
-                          case arg of
-                            Id x -> eval body $ M.insert x param closed_env
-                            _ -> Nothing
-
+eval (Apply f p) env = 
+    eval p env >>= \param ->
+    eval f env >>= \(Closure arg body closed_env) ->
+    case arg of
+      Id x -> eval body $ M.insert x param closed_env
+      _ -> Nothing
 
